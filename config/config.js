852ad81d4c43b29eccba77b97f8df53942c9ee2e
Jesse Paris <Jesse.Paris@qlik.com>
2016-07-13 15:23:42 -0700

    cleaned up config.

--- config/config.js
+++ config/config.js
@@ -1,27 +1,8 @@
 var path = require('path');
-var extend = require('extend');
 
-//For production
+var certPath = __dirname + '/../certs';
 
-
-var certPath = 'F:/My Documents/_Git/global-metrics-app/certs';
-
-//var certPath = path.join(process.env.programdata, '/Qlik/Sense/Repository/Exported Certificates/.Local Certificates');
-var routePath = path.join(__dirname, '/../routes/');
-var publicPath = path.join(__dirname, '/../public/');
-var logPath = path.join(__dirname,'/../log/');
-var appPath = path.join(__dirname, '/../app/');
-
-var logFile = logPath + 'masterlib.log';
-
-var config = extend(true, {
-	port: 8591,
-	enginePort: 4747,
-	qrsPort: 4242,
-	hostname: 'masterlib.112adams.local',
-	repoAccount: 'UserDirectory=Internal;UserId=sa_repository',
-	repoAccountUserDirectory: 'INTERNAL',
-	repoAccountUserId: 'sa_repository',
+var config = {
 	certificates: {
 		client: path.resolve(certPath, 'client.pem'),
 		client_key: path.resolve(certPath,'client_key.pem'),
@@ -29,12 +10,11 @@ var config = extend(true, {
 		server_key: path.resolve(certPath, 'server_key.pem'),
 		root: path.resolve(certPath,'root.pem')
 	},
-	routePath: routePath,
-	publicPath: publicPath,
-	appPath: appPath,
-	logPath: logPath,
-	logFile: logFile,
-	logLevel: 'info'
-});
+	engine: {
+		hostname: 'masterlib.112adams.local',
+		enginePort: 4747,
+		repoAccount: 'UserDirectory=Internal;UserId=sa_repository'
+	}
+}
 
 module.exports = config;
