5c0c8718cf1a9143fcccf39cf4da2db2c1a30e9d
Jesse Paris <Jesse.Paris@qlik.com>
2016-08-08 07:58:52 -0700

    cleaned up data, tabs don't work.

--- main.js
+++ main.js
@@ -12,6 +12,18 @@ var serializeApp = require('serializeapp');
 
 
 var main = function main(qsocks, config){
+    if (!String.prototype.startsWith) {
+        String.prototype.startsWith = function(searchString, position) {
+            position = position || 0;
+            return this.substr(position, searchString.length) === searchString;
+        };
+    }
+
+    String.prototype.replaceAll = function(search, replacement) {
+        var target = this;
+        return target.replace(new RegExp(search, 'g'), replacement);
+    };
+
     // create folder if it doesn't exist
     try {
         fs.mkdirSync(config.filenames.outputDir);
