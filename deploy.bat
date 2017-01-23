4dee81336eef09ae522444ac66c2c745c8d1b34d
Jesse Paris <jparis@gmail.com>
2017-01-23 11:29:43 -0800

    adding files to deploy on osx.

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,9 @@
-xcopy appMetaFetcher ..\QlikSenseQMCUtility\plugins\appMetaFetcher /I /Y /R /S /EXCLUDE:exclusionList.txt
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    rsync -r ./appMetaFetcher ../QlikSenseQMCUtility/plugins/ --exclude */node_modules/
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Linux not supported."
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    xcopy appMetaFetcher ..\QlikSenseQMCUtility\plugins\appMetaFetcher /I /Y /R /S /EXCLUDE:exclusionList.txt
+fi
