cb68311f2b0b49cf4eb23e68c643f66b6074aa55
Jesse Paris <jparis@gmail.com>
2017-01-24 09:51:10 -0800

    fixing deploy for osx

--- deployScripts/copyNPMPackages_osx.sh
+++ deployScripts/copyNPMPackages_osx.sh
@@ -4,6 +4,4 @@ if [ "$(uname)" == "Darwin" ]; then
     rsync -r ./appMetaFetcher/node_modules ../QlikSenseQMCUtility/plugins/appMetaFetcher
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Linux not supported."
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    xcopy appMetaFetcher\node_modules ..\QlikSenseQMCUtility\plugins\appMetaFetcher\node_modules /I /Y /R /S
 fi
