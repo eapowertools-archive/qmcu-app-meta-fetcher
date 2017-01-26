f565586568d3ff4e4173309a2d5f2fc711a2aec8
Jesse Paris <jparis@gmail.com>
2017-01-26 11:12:54 -0800

    code cleanup

--- appMetaFetcher/routes.js
+++ appMetaFetcher/routes.js
@@ -4,10 +4,12 @@ var express = require('express');
 var fs = require('fs');
 var promise = require('bluebird');
 var qrsInteract = require('qrs-interact');
-var socket = require('socket.io-client')('https://localhost:9945', {secure: true, reconnect: true});
+var socket = require('socket.io-client')('https://localhost:9945', {
+    secure: true,
+    reconnect: true
+});
 
 socket.emit("appMetaFetcher", "This is my app metaFetcher message.");
-
 
 var parseUrlencoded = bodyParser.urlencoded({
     extended: false
@@ -18,6 +20,5 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
     extended: true
 }));
-
 
 module.exports = router;
