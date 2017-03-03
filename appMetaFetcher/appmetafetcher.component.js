2c81bad2490cb5977a78f5beebf479bef324b2e8
Jeffrey Goldberg <goldbergjeffrey@yahoo.com>
2017-03-03 15:15:53 -0500

    Updated component paths to support security in qmcu

--- appMetaFetcher/appmetafetcher.component.js
+++ appMetaFetcher/appmetafetcher.component.js
@@ -1,7 +1,7 @@
-(function () {
+(function() {
     "use strict";
     var module = angular.module("QMCUtilities", ["ngDialog", "btford.socket-io"])
-        .factory('mySocket', function (socketFactory) {
+        .factory('mySocket', function(socketFactory) {
             return socketFactory();
         });
 
@@ -10,22 +10,22 @@
         model.statusOutput = '';
         model.exportPath = 'C:\\Program Files\\Qlik\\Sense\\EAPowerTools\\GMAOutput';
 
-        mySocket.on("appMetaFetcher", function (msg) {
+        mySocket.on("appMetaFetcher", function(msg) {
             model.statusOutput += msg + "\n";
         });
 
-        model.isTriggerButtonValid = function () {
+        model.isTriggerButtonValid = function() {
             return true;
         }
 
-        model.triggerMetaFetcher = function () {
-            $http.post('/appmetafetcher/fetch', {
+        model.triggerMetaFetcher = function() {
+            $http.post('./appmetafetcher/fetch', {
                 "exportPath": model.exportPath
             });
             return;
         };
 
-        model.openHelp = function () {
+        model.openHelp = function() {
             ngDialog.open({
                 template: "plugins/appMetaFetcher/help-dialog.html",
                 className: "help-dialog",
